a501fc3cdd5947a4ad7db44cfdebb53eef73b191
SaidaBinta87 <oyshbinte@gmail.com>
2023-09-22 12:46:55 -0700

    Admin Dashboard

---
+++ WebApplication1MyCV/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1MyCV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Education Information";
+
+            return View();
+        }
+        public ActionResult ProjectInfo()
+        {
+            return View();
+
+        }
+      
+
+        public ActionResult Reference()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1MyCV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication1MyCV.MvcApplication" Language="C#" %>

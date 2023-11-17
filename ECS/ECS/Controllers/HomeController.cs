5152bfa1d4314e228cf9f5f59c10b858737104de
SaidaBinta87 <oyshbinte@gmail.com>
2023-11-17 13:55:33 +0600

    Ecommerce

---
+++ ECS/ECS/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using ECS.EF;
+using ECS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AllOrders()
+        {
+            var db = new EcommerceEntities1();
+            return View(db.Orders.ToList());
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var db = new EcommerceEntities1();
+            var order = db.Orders.Find(id);
+            return View(order);
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+      
+        public ActionResult Login(LoginModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                EcommerceEntities1 db = new EcommerceEntities1();
+                var user = (from u in db.Users
+                            where u.username.Equals(login.Username)
+                            && u.passowrd.Equals(login.Password)
+                            select u).SingleOrDefault();
+                if (user != null)
+                {
+                    Session["user"] = user;
+                    var returnUrl = Request["ReturnUrl"];
+                    if (returnUrl != null)
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Order");
+                }
+                TempData["Msg"] = "Username Password Invalid";
+            }
+            return View(login);
+
+        }
+
+        public ActionResult Process(int id)
+        {
+            EcommerceEntities1 ctx = new EcommerceEntities1();
+            var order = ctx.Orders.Find(id);
+            foreach (var od in order.OrderItems)
+            {
+                
+                int Qty = 1000;
+                Qty -= od.Quantity;
+            }
+            
+            ctx.SaveChanges();
+            TempData["Msg"] = "Orded Placed Successfully";
+            return RedirectToAction("AllOrders");
+
+        }
+
+        public ActionResult Cancel(int id)
+        {
+            EcommerceEntities1 db = new EcommerceEntities1();
+            //var order = db.Orders.Find(id);
+            //order.Status = "Cancelled by Admin";
+            db.SaveChanges();
+            TempData["Msg"] = "Orded Cancelled";
+            return RedirectToAction("AllOrders");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ECS/ECS/Controllers/OrderController.cs
@@ -0,0 +1,106 @@
+﻿using ECS.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECS.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(int id = 1)
+        {
+
+            var db = new EcommerceEntities1();
+            int itemperpage = 20;
+            var products = db.Products.OrderBy(e => e.productID).Skip((id - 1) * itemperpage).Take(itemperpage).ToList();
+
+            var pagescount = Math.Ceiling(db.Products.Count() / (decimal)itemperpage);
+            ViewBag.Pages = pagescount;
+            return View(products);
+
+
+        }
+
+        public ActionResult AddCart(int id)
+        {
+            EcommerceEntities1 db = new EcommerceEntities1();
+            List<Product> cart = null;
+            if (Session["cart"] == null)
+            {
+                cart = new List<Product>();
+            }
+            else
+            {
+                cart = (List<Product>)Session["cart"];
+            }
+
+            var product = db.Products.Find(id);
+
+            cart.Add(new Product()
+            {
+                productID = product.productID,
+                Pname = product.Pname,
+                Category = product.Category,
+                Price = product.Price,
+            });
+            Session["cart"] = cart;
+
+            TempData["Msg"] = "Successfully Added";
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Cart()
+        {
+
+            var cart = (List<Product>)Session["cart"];
+            if (cart != null)
+            {
+                return View(cart);
+            }
+            TempData["Msg"] = "Cart Empty";
+            return RedirectToAction("Index");
+
+            //order.Date = DateTime.Now;
+        }
+        public ActionResult Place()
+        {
+            if (Session["user"] == null)
+            {
+                // Handle the case where the user is not logged in
+                return RedirectToAction("Login");
+            }
+            int userID = ((User)Session["user"]).UserID;
+            int orderSum = 0;
+            var cart = (List<Product>)Session["cart"];
+            Order order = new Order();
+            order.Useid = userID;
+
+            order.Amount = 0;
+            EcommerceEntities1 db = new EcommerceEntities1();
+            db.Orders.Add(order);
+            db.SaveChanges();
+            foreach (var p in cart)
+            {
+                var od = new OrderItem();
+                od.Quantity = +1;
+               od.OID = order.orderID;
+                od.PID = p.productID;
+                od.Subtotal = (int)p.Price;
+                od.Quantity = 1;
+                orderSum += (1 * (int)p.Price);
+                db.OrderItems.Add(od);
+            }
+            order.Amount = orderSum;
+            db.SaveChanges();
+            Session["cart"] = null;
+            TempData["Msg"] = "Order Placed Successfully";
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
